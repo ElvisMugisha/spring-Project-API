d0f7810cb97d0e2f8405d2e9f32d504817d33d98
ElvisMugisha <mugishaelvis104@gmail.com>
2022-11-03 14:58:13 +0200

    Initial commit

---
+++ src/main/java/com/springAPI/spring/Project/API/SpringProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.springAPI.spring.Project.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springAPI/spring/Project/API/SpringProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springAPI.spring.Project.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
